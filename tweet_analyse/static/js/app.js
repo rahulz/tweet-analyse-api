4a4a91736acdbf0f52a0a4c13057d461d87a8543
Rahul <rahul.k@cubettech.com>
2018-04-10 16:23:54 +0530

    ui fixes, tweet list

--- tweet_analyse/static/js/app.js
+++ tweet_analyse/static/js/app.js
@@ -1,4 +1,4 @@
-analyse = angular.module('analyse', ['ngWebSocket']);
+analyse = angular.module('analyse', ['ngWebSocket', 'ui.grid']);
 
 analyse.factory('Socket', function ($websocket) {
     var dataStream = $websocket('ws://' + window.location.host + '/ws');
@@ -14,9 +14,24 @@ analyse.factory('Socket', function ($websocket) {
     return methods;
 });
 analyse.controller('Controller', function PhoneListController($scope, $timeout, $http, Socket) {
+    // $('.overlay').show();
+
     $http.get('/trends').then(function (data) {
         $scope.trends = data.data;
     });
+    $scope.tweetGridOptions = {
+        enableSorting: true,
+        columnDefs: [
+            {field: 'created_at'},
+            {field: 'id'},
+            {field: 'text'},
+            {field: 'user.name', name: 'User'},
+            {field: 'retweeted_status.retweet_count', name: 'Retweet count'},
+            {field: 'retweeted_status.favorite_count', name: 'Favorite count'},
+
+
+        ]
+    };
 
     Socket.socket.onMessage(function (message) {
         $scope.status = JSON.parse(message.data);
@@ -24,11 +39,12 @@ analyse.controller('Controller', function PhoneListController($scope, $timeout, 
         if ($scope.status.progress === 100) {
             $timeout(function () {
                 $scope.result = $scope.status.result;
-                console.log($scope.result.sentiment.counts);
+                console.log($scope.result.tweets);
 
                 // document.getElementsByTagName('body')[0].style.backgroundImage = "url(" + $scope.result.wordcloud + ")"
                 $scope.status = undefined;
                 var ctx = document.getElementById('canvas').getContext('2d');
+                $scope.tweetGridOptions.data = $scope.result.tweets;
                 var myChart = new Chart(ctx, {
                     type: 'pie',
                     data: {
@@ -43,7 +59,17 @@ analyse.controller('Controller', function PhoneListController($scope, $timeout, 
                         }
                         ]
                     },
-                    options: {}
+                    options: {
+                        legend: {
+                            labels: {
+                                defaultFontFamily: 'Roboto'
+                            }
+                        },
+                        title: {
+                            display: true,
+                            text: 'Sentiment analysis: ' + $scope.q
+                        }
+                    }
                 });
             }, 1000)
         }
@@ -55,4 +81,8 @@ analyse.controller('Controller', function PhoneListController($scope, $timeout, 
         console.log("init");
         Socket.send({q: $scope.q, action: "generate"});
     }
+    $('.overlay').slideUp('fast', function () {
+        $('.overlay').css({'opacity': 1});
+
+    });
 });
